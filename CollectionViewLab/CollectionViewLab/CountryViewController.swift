8e435969edd5f077995ae04bd1282968002cb7a6
maitreebain <maitreebain@gmail.com>
2020-01-15 16:54:22 -0500

    populating collection view + adding search bar

--- CollectionViewLab/CollectionViewLab/CountryViewController.swift
+++ CollectionViewLab/CollectionViewLab/CountryViewController.swift
@@ -10,14 +10,68 @@ import UIKit
 
 class CountryViewController: UIViewController {
     
+    @IBOutlet weak var countryCollection: UICollectionView!
     
+    var country = [CountryData](){
+        didSet {
+            DispatchQueue.main.async {
+                self.countryCollection.reloadData()
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        countryCollection.dataSource = self
+        countryCollection.delegate = self
         
     }
 
+    func loadData() {
+        
+    }
 
 }
 
+extension CountryViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return country.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "countryCell", for: indexPath) as? CountryCell else {
+            fatalError("cell not conforming")
+        }
+        
+        let selectedCountry = country[indexPath.row]
+        cell.configureCell(for: selectedCountry)
+        return cell
+    }
+
+}
+ 
+extension CountryViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let itemSpacing: CGFloat = 1
+        let maxWidth = UIScreen.main.bounds.size.width
+        let numberOfItems: CGFloat = 1
+        let totalSpace: CGFloat = numberOfItems * itemSpacing
+        let itemWidth: CGFloat = (maxWidth - totalSpace) / numberOfItems
+        
+        return CGSize(width: itemWidth, height: itemWidth)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        return UIEdgeInsets(top: 20, left: 1, bottom: 20, right: 1)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+}
